0af840eeba9896917d81a76ab377911034b8ba33
Harshit Pachar <harsitpachar@gmail.com>
2023-08-19 12:51:11 +0530

    Initial Commit

--- src/components/wallet/WalletBox.js
+++ src/components/wallet/WalletBox.js
@@ -8,24 +8,45 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
 
 const WalletBox = () => {
+  const navigate = useNavigate();
   return (
     <Box sx={{ bgcolor: "black", height: "100%", width: "30%" }}>
-      <Card sx={{ maxWidth: 345 }}>
+      <Button variant="outlined" onClick={() => {navigate("/")}} sx={{mt: 1.5, ml: 1.5}} startIcon={<ArrowBackIcon />}>
+        Back
+      </Button>
+      <Card sx={{ maxWidth: 330, ml: 7, mt: 7 }}>
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+          <Typography gutterBottom variant="h4" component="div" sx={{display: "flex", justifyContent: "center"}}>
+            Total Tokens
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography variant="h5" color="text.secondary" sx={{display: "flex", justifyContent: "center"}}>
+            2000
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+      </Card>
+      <Card sx={{ maxWidth: 330, ml: 7, mt: 7 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div" sx={{display: "flex", justifyContent: "center"}}>
+            Total Tokens
+          </Typography>
+          <Typography variant="h5" color="text.secondary" sx={{display: "flex", justifyContent: "center"}}>
+            2000
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ maxWidth: 330, ml: 7, mt: 7 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div" sx={{display: "flex", justifyContent: "center"}}>
+            Total Tokens
+          </Typography>
+          <Typography variant="h5" color="text.secondary" sx={{display: "flex", justifyContent: "center"}}>
+            2000
+          </Typography>
+        </CardContent>
       </Card>
     </Box>
   );
